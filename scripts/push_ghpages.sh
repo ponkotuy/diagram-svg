404d32b98dbfd771e8de868753104f1164b723b3
ponkotuy <web@ponkotuy.com>
2020-02-01 23:52:38 +0900

    Fix push_ghpages

--- scripts/push_ghpages.sh
+++ scripts/push_ghpages.sh
@@ -4,6 +4,7 @@ git config --global user.name "ponkotuy-bot"
 git config --global user.email "web@ponkotuy.com"
 
 git checkout -b gh-pages
+git merge master
 git add -A
 git commit -m "Auto push gh-pages"
 git push -f origin gh-pages
