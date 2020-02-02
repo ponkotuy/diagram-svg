1c0c3357d9dff1316069b5249cbed9d301ef51f4
ponkotuy <web@ponkotuy.com>
2020-02-02 22:12:57 +0900

    Fix CI

--- scripts/push_ghpages.sh
+++ scripts/push_ghpages.sh
@@ -2,4 +2,4 @@
 
 git add -A
 git commit -m "Auto push gh-pages"
-git push origin gh-pages
+git push -f origin HEAD:gh-pages
--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -3,6 +3,5 @@
 git config --global user.name "ponkotuy-bot"
 git config --global user.email "web@ponkotuy.com"
 
-git fetch
-git checkout origin/gh-pages
-git merge origin/master
+git checkout gh-pages
+git merge origin/master -m "Merge master"
