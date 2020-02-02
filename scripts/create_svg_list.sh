3957a52bc4c2fffc011f1a8e0b2b37f4ad42e03e
ponkotuy <web@ponkotuy.com>
2020-02-02 21:00:54 +0900

    Fix create_svg_list.sh

--- scripts/create_svg_list.sh
+++ scripts/create_svg_list.sh
@@ -5,7 +5,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-echo -n '['
+echo '['
 first=1
 for file in `find $1 -maxdepth 1 -name "*.json"`; do
     if [ $first -eq 0 ]; then
@@ -13,7 +13,7 @@ for file in `find $1 -maxdepth 1 -name "*.json"`; do
     fi
     title=$(cat $file | jq .title)
     author=$(cat $file | jq .author)
-    echo -n "{\"file\":\"$file\", \"name\":$title, \"author\":$author}"
+    echo "{\"file\":\"$file\", \"name\":$title, \"author\":$author}"
     first=0
 done
-echo "]"
+echo ']'
