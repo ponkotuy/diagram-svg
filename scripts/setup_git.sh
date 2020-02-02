37ac0907413af3b6e7163ad260c41df967d83ee1
ponkotuy <web@ponkotuy.com>
2020-02-02 21:20:16 +0900

    Fix TS Build

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -3,6 +3,6 @@
 git config --global user.name "ponkotuy-bot"
 git config --global user.email "web@ponkotuy.com"
 
-git checkout -b gh-pages
+git checkout origin/gh-pages
 git merge master
 rm .gitignore
