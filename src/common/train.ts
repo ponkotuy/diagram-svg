d4957f5cdb8d1359f458843d257afd80d7cd5559
ponkotuy <web@ponkotuy.com>
2020-03-18 23:49:15 +0900

    Add train name & refine JR Kyusyu Line

--- src/common/train.ts
+++ src/common/train.ts
@@ -1,16 +1,16 @@
-import {TrainStops} from "../csv2json/trainStops";
-
 export class Train {
   readonly stations: number[];
   readonly branches: number[][];
   readonly count: number;
   readonly speed: Speed;
+  readonly name: string | null;
 
   constructor(obj: TrainObj) {
     this.stations = obj.stations;
     this.branches = obj.branches || [];
     this.count = obj.count;
-    this.speed = obj.speed
+    this.speed = obj.speed;
+    this.name = obj.name;
   }
 }
 
@@ -19,6 +19,7 @@ export type TrainObj = {
   branches: number[][] | null;
   count: number
   speed: Speed
+  name: string | null
 }
 
 export type Speed = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10
